f66ee0e7a02829b864f28c60b55d8aaf95a2dbc8
Nickolai Tchesnokov <nickolaitchesnokov@Nickolais-MacBook-Air-543.local>
2023-02-20 15:27:10 +0000

    terminal inputs added

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ from stable_baselines3.common.evaluation import evaluate_policy
 # Initialize environment
 start_pos = (0, 0) # set start state of agent
 n_channels = 2       # Agent pos channel & agent path channel
-width = 5
-height = 5
+width = 3
+height = 3
 
 episodes = 100_000 # Number of times environment is run
 n_split = 10 # Split episode outputs into this number
@@ -51,13 +51,29 @@ if __name__ == "__main__":
     env = GridEnv(width, height, n_channels, start_pos)
     env.reset()
 
-    models_dir = "models/3x3/A2C"
     logdir = "logs/3x3"
-    load_steps = 10000
-    model_path = f"{models_dir}/3x3/{load_steps}"
-    # run_ppo(env, models_dir, logdir, episodes)
-    run_a2c(env, models_dir, logdir, episodes)
-    # load_model(model_path, env)
+
+    model_type = input("Select type (DQN, PPO, A2C, NNA or load): ")
+
+    if model_type == "PPO":
+        models_dir = "models/3x3/PPO"
+        run_ppo(env, models_dir, logdir, episodes)
+
+    if model_type == "A2C":
+        models_dir = "models/3x3/A2C"
+        run_a2c(env, models_dir, logdir, episodes)
+    
+    if model_type == "DQN":
+        models_dir = "models/3x3/DQN"
+        # DQN function here ...
+
+    if model_type == "load":
+        model_type = input("Select type (DQN, PPO, A2C, NNA): ")
+        load_steps = input("Load model at timestep: ")
+        models_dir = f"models/3x3/{model_type}"
+        model_path = f"{models_dir}/{load_steps}"
+
+        load_model(model_path, env)
 
     env.close()
 
